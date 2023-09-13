5e3c6a8896df2695a5e35e03e69270d8b225e58a
bassembuto <132224648+bassembuto@users.noreply.github.com>
2023-09-13 18:41:18 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+
+let pound = prompt('من فضلك أدخل المبلغ بالدولار لتحويلة إلي جنية مصري');
+function age() {
+    let finish = pound * 30.90;
+    console.log(`عدد الجنيهات هي : ${finish.toFixed(2)}`)
+    document.write(`عدد الجنيهات هي : ${finish.toFixed(2)}`);
+}
+age();
